8954a1de8c8ab2626b18334483dac7dbc7b05ba9
aubyoung <aubyoung@deloitte.com>
2019-06-11 15:07:23 -0400

    Update PowerShell script to get INSERT statements from queries.txt file

--- AutomatedCreationOfDatabase/SQLite/scripts/createdb.ps1
+++ AutomatedCreationOfDatabase/SQLite/scripts/createdb.ps1
@@ -1,8 +1,4 @@
- #This PowerShell script creates a small SQLite DB with a table called "NAMES"
- #It populates the table with some data for testing
-
-
- #Import the module, and create the table
+ #Import the module, create a data source and a table
  Import-Module PSSQLite
 
    $Database = "C:\Names.SQLite"
@@ -13,57 +9,16 @@
         Givenname TEXT,
         Birthdate DATETIME)"
 
-
     #SQLite will create Names.SQLite for us
 
     Invoke-SqliteQuery -Query $Query -DataSource $Database
 
-    $query = "INSERT INTO NAMES (Fullname, Surname, Givenname, Birthdate)
-                          VALUES (@full, 'Cookie', 'Monster', @BD)"
+# Loop through the lines in the text file
+$queries = Get-Content ("queries.txt")
 
-    Invoke-SqliteQuery -DataSource $Database -Query $query -SqlParameters @{
-        full = "Cookie Monster"
-        BD   = (get-date).addyears(-3)
-    }
+For ($i=0; $i -lt $queries.Length; $i++) {
+	$query = $queries[$i];
 
-	$query = "INSERT INTO NAMES (Fullname, Surname, Givenname, Birthdate)
-                          VALUES (@full, @first, @last, @BD)"
-
-	Invoke-SqliteQuery -DataSource $Database -Query $query -SqlParameters @{
-        full = "Michael Jackson"
-		first = "Michael"
-		last = "Jackson"
-        BD = '1958-08-29'
-    }
-
-	$query = "INSERT INTO NAMES (Fullname, Surname, Givenname, Birthdate)
-                          VALUES (@full, @first, @last, @BD)"
-
-	Invoke-SqliteQuery -DataSource $Database -Query $query -SqlParameters @{
-        full = "Jane Doe"
-		first = "Jane"
-		last = "Doe"
-        BD = '1959-08-29'
-    }
-
-
-	$query = "INSERT INTO NAMES (Fullname, Surname, Givenname, Birthdate)
-                          VALUES (@full, @first, @last, @BD)"
-
-	Invoke-SqliteQuery -DataSource $Database -Query $query -SqlParameters @{
-        full = "John Doe"
-		first = "John"
-		last = "Doe"
-        BD = '1960-08-29'
-    }
-
-
-	$query = "INSERT INTO NAMES (Fullname, Surname, Givenname, Birthdate)
-                          VALUES (@full, @first, @last, @BD)"
-
-	Invoke-SqliteQuery -DataSource $Database -Query $query -SqlParameters @{
-        full = "Jackie Onassis"
-		first = "Jackie"
-		last = "Onassis"
-        BD = '1929-07-28'
-    }
+	"Line $i, the query is  $query"
+	Invoke-SqliteQuery -DataSource $Database -Query $query
+}
